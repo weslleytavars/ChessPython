34ef5147a3d5ab83b8bf7a08128471849da60c2c
weslleytavars <weslleytavares2009@gmail.com>
2025-08-22 20:51:38 -0300

    feat: Application window open in screen center

--- src/view/app.py
+++ src/view/app.py
@@ -6,3 +6,19 @@ class App(Tk):
         self.title("ChessPython")
         self.geometry("800x600")
         self.resizable(False, False)
+
+        self.center_window()
+
+    def center_window(self):
+        """Center the window on the screen."""
+        self.update_idletasks()
+
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        app_width = self.winfo_width()
+        app_height = self.winfo_height()
+
+        x = (screen_width // 2) - (app_width // 2)
+        y = (screen_height // 2) - (app_height // 2)
+        self.geometry(f"{app_width}x{app_height}+{x}+{y}")
